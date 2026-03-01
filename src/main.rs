b96c6f892b5f15ec324ce29956abc0bf409987f3
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2026-03-01 09:29:54 +0100

    daisyUI installed

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod views;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
+const DAISY_CSS: Asset = asset!("/assets/daisy.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
@@ -18,6 +19,7 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        document::Link { rel: "stylesheet", href: DAISY_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
         Router::<Route> {}
     }
 }
